29bafc21a0828ee3cc94eb62d8ffd876d7ac8fba
fbssistemas <fbssistemas>
2021-01-26 20:22:39 -0300

    Groups TDD

---
+++ app/Http/Controllers/GroupController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Group;
+use Illuminate\Http\Request;
+
+class GroupController extends Controller
+{
+    public function index()
+    {
+        return Group::all();
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        $data['cnpj'] = preg_replace('/\D/', '', $data['cnpj']);
+        return Group::create($data);
+    }
+}
